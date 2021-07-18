13d69fae951b7b955ca193eff7fcdc465c6b3d32
livrbecca <ollywalker_1998@outlook.com>
2021-07-18 15:11:07 +0100

    all functions moved to utils

--- src/components/BabyList.tsx
+++ src/components/BabyList.tsx
@@ -1,29 +1,18 @@
 import data from "../data/babyNamesData.json";
 import { useState } from "react";
 import { BabyDataProps } from "../utils/interfaces";
-import Baby from "./Baby";
+// import Baby from "./Baby";
 import SearchBar from "./SearchBar";
+import { renderMapHTML } from "../utils/functions";
 
 const BabyList: React.FC = () => {
   const [babyData, setBabyData] = useState<BabyDataProps[]>(data);
   const [searchTerm, setSearchTerm] = useState<string>("");
 
-  const searchBar = babyData.filter((value) =>
-    value.name.toLocaleLowerCase().includes(searchTerm)
-  );
-
-  const renderMapHTML = (): JSX.Element[] => {
-    return searchBar
-      .sort((a, b) => a.name.localeCompare(b.name))
-      .map((value) => {
-        return <Baby name={value.name} id={value.id} sex={value.sex} />;
-      });
-  };
-
   return (
     <>
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <div className="container">{renderMapHTML()}</div>;
+      <div className="container">{renderMapHTML(babyData, searchTerm)}</div>;
     </>
   );
 };
--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { BabyDataProps } from "./interfaces";
+import Baby from "../components/Baby";
 
-const functions = () => {
-  return <div></div>;
+export const renderMapHTML = (
+  babyData: BabyDataProps[],
+  searchTerm: string
+): JSX.Element[] => {
+  return searchBar(babyData, searchTerm)
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .map((value) => {
+      return <Baby name={value.name} id={value.id} sex={value.sex} />;
+    });
 };
 
-export default functions;
+export const searchBar = (
+  babyData: BabyDataProps[],
+  searchTerm: string
+): BabyDataProps[] => {
+  return babyData.filter((value) =>
+    value.name.toLocaleLowerCase().includes(searchTerm)
+  );
+};
